47f016f8ad5139fc12108f71c602379ecc74d6fc
Renato Diniz <renatodiniz25@gmail.com>
2023-12-08 11:27:38 -0300

    uncomment rules for part one

--- src/day05.rs
+++ src/day05.rs
@@ -16,13 +16,13 @@ pub fn resolve() {
     let entry = line.unwrap();
 
     // Set of rules for Part One
-    // let contains_at_least_three_vowels = Regex::new(r"[aeiou].*[aeiou].*[aeiou]").unwrap();
-    // let contain_certain_substrings = RegexSet::new(&[r"ab", r"cd", r"pq", r"xy",]).unwrap();
-    // let has_consecutive_letters = Regex::new(r"([a-zA-Z])\1").unwrap();
+    let contains_at_least_three_vowels = Regex::new(r"[aeiou].*[aeiou].*[aeiou]").unwrap();
+    let contain_certain_substrings = RegexSet::new(&[r"ab", r"cd", r"pq", r"xy",]).unwrap();
+    let has_consecutive_letters = Regex::new(r"([a-zA-Z])\1").unwrap();
 
-    // if !contains_at_least_three_vowels.is_match(&entry).unwrap() { continue };
-    // if contain_certain_substrings.is_match(&entry) { continue };
-    // if !has_consecutive_letters.is_match(&entry).unwrap() { continue };
+    if !contains_at_least_three_vowels.is_match(&entry).unwrap() { continue };
+    if contain_certain_substrings.is_match(&entry) { continue };
+    if !has_consecutive_letters.is_match(&entry).unwrap() { continue };
 
     // Set of rules for Part Two
     let letter_repeats_with_one_letter_in_between = Regex::new(r"([a-zA-Z])\w\1").unwrap();
@@ -34,5 +34,5 @@ pub fn resolve() {
     counter += 1;
   }
 
-  println!("{counter}");
+  println!("There are {counter} nice strings.");
 }
